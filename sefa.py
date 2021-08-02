38cb002b393bb61c9fd971d2fdb9e2991b004245
Hyung-Kwon Ko <hyungkwonko@gmail.com>
2021-08-02 06:54:04 +0000

    sefa refactoring

--- sefa.py
+++ sefa.py
@@ -1,38 +1,55 @@
-import argparse
+import os
 import pickle
 import torch
-
+import argparse
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="sefa")
-    parser.add_argument("--out", type=str, default="factor.pt", help="name of the result factor file")
-    # parser.add_argument("--ckpt", type=str, default="pretrained_models/webtoon001693.pkl", help="model checkpoint")
-    parser.add_argument("--ckpt", type=str, default='../stylegan2-ada-pytorch/results/00005-webtoon_3channel-webtoon256-bgcfnc-resumeffhq256/models/network-snapshot-007338.pkl', help="model checkpoint")
-    
+    parser.add_argument("--feature", default='all', choices=['low', 'middle', 'high', 'all'])
+    parser.add_argument("--mtype", default='default', choices=['default', 'mixing', 'xflip'])
+
     args = parser.parse_args()
 
-    with open(args.ckpt, 'rb') as f:
+    if args.mtype == 'default':
+        ckpt = os.path.join('pretrained_models', 'network-snapshot-008467.pkl')
+    elif args.mtype == 'mixing':
+        ckpt = os.path.join('pretrained_models', 'network-snapshot-mixing-010080.pkl')
+    elif args.mtype == 'xflip':
+        ckpt = os.path.join('pretrained_models', 'network-snapshot-xflip-004273.pkl')
+    else:
+        raise AssertionError('check pretrained model type')
+
+    with open(ckpt, 'rb') as f:
         obj = f.read()
-    ckpt = pickle.loads(obj, encoding='latin1')
+    model = pickle.loads(obj, encoding='latin1')
 
     weights = []
     names = []
 
-    for name in ckpt["G_ema"].state_dict():
+    if args.feature == 'low':
+        layers = ['b4', 'b8', ]
+    elif args.feature == 'middle':
+        layers = ['b16', 'b32', 'b64']
+    elif args.feature == 'high':
+        layers = ['b128', 'b256']
+    else:
+        layers = ['']
+
+    for name in model["G_ema"].state_dict():
         if ("synthesis" in name) and ("torgb" not in name) and ("affine.weight" in name):
-            weight = ckpt["G_ema"].state_dict()[name]
-            weights.append(weight)
-            names.append(name)
-            print(f"[INFO] append {len(names)}: {name}... {weight.shape}")
-    # for name in ckpt.state_dict():
-    #     if ("synthesis" in name) and ("torgb" not in name) and ("affine.weight" in name):
-    #         weight = ckpt.state_dict()[name]
-    #         weights.append(weight)
-    #         names.append(name)
-    #         print(f"[INFO] append {len(names)}: {name}... {weight.shape}")
+            for layer in layers:
+                if layer in name:
+                    weight = model["G_ema"].state_dict()[name]
+                    weights.append(weight)
+                    names.append(name)
+                    print(f"[INFO] append {len(names)}: {name}... {weight.shape}")
+                    break
+
     W = torch.cat(weights, 0)
     print(f"[INFO] concatenated W.shape: {W.shape}")
 
+    save_name = f'{args.mtype}-{args.feature}.pt'
+    out_file = os.path.join('pretrained_models', save_name)
     eigvec = torch.svd(W).V.to("cpu")
-    torch.save({"ckpt": args.ckpt, "eigvec": eigvec}, args.out)
-    print(f"[INFO] saved successfully to {args.out}")
+    torch.save({"filename": save_name, "eigvec": eigvec}, out_file)
+    print(f"[INFO] saved successfully to {out_file}")
